f41540870c220af1db2411fd39385868d9269e2e
Kiril <kiril.krumov@sap.com>
2023-03-10 12:35:26 +0200

    Initial commit

---
+++ test/banking/AccountTest.java
@@ -0,0 +1,25 @@
+package banking;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.Theories;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Theories.class)
+public class AccountTest {
+
+    private Account sut;
+
+    @Before
+    public void setUp() {
+        sut = new Account();
+    }
+
+    @Test
+    public void shouldCreateANewAccount() {
+        assertNotNull(sut);
+    }
+
+}
